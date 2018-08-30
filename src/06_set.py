27317dd71c65efa8158ec38383f2e30725b24339
c10t <c10t@fa.ke>
2018-08-31 08:35:01 +0900

    n-gram

---
+++ src/06_set.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+def char_bigram_as_set(word):
+    return {(word[i], word[i + 1]) for i in range(len(word)) if i < len(word) - 1}
+
+
+def main():
+    x = char_bigram_as_set("paraparaparadise")
+    y = char_bigram_as_set("paragraph")
+    print("X := {}".format(x))
+    print("Y := {}".format(y))
+    print("X | Y = {}".format(x | y))
+    print("X & Y = {}".format(x & y))
+    print("X - Y = {}".format(x ^ y))
+    print("Is 'se' in X? - {}".format(""))
+    print("Is 'se' in Y? - {}".format(if ''))
+
+
+if __name__ == '__main__':
+    main()
