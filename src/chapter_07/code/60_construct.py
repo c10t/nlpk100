04c84d519511893aa998c3b056cbdd80d8d246a1
c10t <c10t@fa.ke>
2018-12-11 23:40:14 +0900

    processing json

--- src/chapter_07/code/60_construct.py
+++ src/chapter_07/code/60_construct.py
@@ -1,5 +1,34 @@
-import redis
+import json
 
-pool = redis.ConnectionPool(host='myredis', port=6379, db=0)
-r = redis.StrictRedis(connection_pool=pool)
+ARTIST_PATH = "../resources/artist.json"
+OUTPUT_PATH = "../resources/name-area.txt"
 
+def dict_to_line(d):
+    if "name" not in d.keys():
+        return ""
+
+    if "area" in d.keys():
+        return 'SET "name:{}" "area:{}"'.format(d['name'], d['area'])
+    else:
+        return 'SET "name:{}" "area:{}"'.format(d['name'], "---")
+
+
+def main():
+    print("Reading JSON file...")
+
+    count = 0
+    with open(ARTIST_PATH, encoding="utf8") as f, open(OUTPUT_PATH, mode='a', encoding='utf8', newline="\r\n") as g:
+        for line in f:
+            # if count > 10:
+            #    break
+            j = json.loads(line)
+            g.write(dict_to_line(j) + '\n')
+            count += 1
+            if count % 10000 == 0:
+                print(f"Processed {count} lines...")
+
+    print(f"Finished with {count} lines")
+
+
+if __name__ == "__main__":
+    main()
