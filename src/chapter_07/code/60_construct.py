56cda6a2826dd3c6af517febcbfcb7833dbd223c
c10t <c10t@fa.ke>
2018-12-12 23:19:07 +0900

    create namelist

--- src/chapter_07/code/60_construct.py
+++ src/chapter_07/code/60_construct.py
@@ -1,9 +1,11 @@
 import json
 
 ARTIST_PATH = "../resources/artist.json"
-OUTPUT_PATH = "../resources/name-area.txt"
+NAMELIST_PATH = "../resources/namelist.txt"
+NAME_TO_AREA_PATH = "../resources/name-area.txt"
 
-def dict_to_line(d):
+
+def dict_to_line_set(d):
     if "name" not in d.keys():
         return ""
 
@@ -13,18 +15,28 @@ def dict_to_line(d):
         return 'SET "name:{}" "area:{}"'.format(d['name'], "---")
 
 
+def dict_to_namelist(d):
+    if "name" not in d.keys():
+        return ""
+
+    return 'RPUSH namelist "{}"'.format(d['name'])
+
+
 def main():
     print("Reading JSON file...")
 
     count = 0
-    with open(ARTIST_PATH, encoding="utf8") as f, open(OUTPUT_PATH, mode='a', encoding='utf8', newline="\r\n") as g:
+    with open(ARTIST_PATH, encoding="utf8") as f,\
+        open(NAMELIST_PATH, mode='a', encoding='utf8', newline="\r\n") as g, \
+        open(NAME_TO_AREA_PATH, mode='a', encoding='utf8', newline="\r\n") as h:
         for line in f:
             # if count > 10:
             #    break
             j = json.loads(line)
-            g.write(dict_to_line(j) + '\n')
+            g.write(dict_to_namelist(j) + '\n')
+            h.write(dict_to_line_set(j) + '\n')
             count += 1
-            if count % 10000 == 0:
+            if count % 100000 == 0:
                 print(f"Processed {count} lines...")
 
     print(f"Finished with {count} lines")
