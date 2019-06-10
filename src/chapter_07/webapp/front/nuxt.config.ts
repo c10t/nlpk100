b2fe2393d0ffbcfce590ee45e3808c4093dc1f17
c10t <c10t@fa.ke>
2019-06-10 23:54:48 +0900

    add express api

--- src/chapter_07/webapp/front/nuxt.config.ts
+++ src/chapter_07/webapp/front/nuxt.config.ts
@@ -8,7 +8,10 @@ const config: NuxtConfiguration = {
     link: [
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500' }
     ]
-  }
+  },
+  serverMiddleware: [
+    { path: '/api', handler: '@/server/api/index.ts' }
+  ]
 }
 
 export default config
