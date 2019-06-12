7a46d5d8c9dc8d01ba54507244d6eb9e76e4a1ca
c10t <c10t@fa.ke>
2019-06-12 23:59:48 +0900

    add appbar

--- src/chapter_07/webapp/front/pages/index.vue
+++ src/chapter_07/webapp/front/pages/index.vue
@@ -1,9 +1,16 @@
 <template>
   <v-app>
     <v-content>
-      <v-container>
-        Hello, Vuetify!
-      </v-container>
+      <v-container>Hello, Vuetify!</v-container>
     </v-content>
   </v-app>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+@Component({
+  components: {
+    Appbar: () => import("@/components/appbar.vue")
+  }
+})
+export default class IndexPage extends Vue {}
+</script>
