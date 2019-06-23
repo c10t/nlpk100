8d49237a498db25fb4b04c1fb643e9f8775101e1
c10t <c10t@fa.ke>
2019-06-23 23:36:06 +0900

    add table

--- src/chapter_07/webapp/front/components/artists-table.vue
+++ src/chapter_07/webapp/front/components/artists-table.vue
@@ -2,6 +2,15 @@
   <v-data-table :headers="headers" :items="artists" class="elevation-1">
     <template v-slot:items="props">
       <td>{{ props.item.name }}</td>
+      <td>{{ props.item.area }}</td>
+      <td>{{ props.item.aliases }}</td>
+      <td>{{ props.item.begin }}</td>
+      <td>{{ props.item.end }}</td>
+      <td>{{ props.item.tags }}</td>
+      <td>
+        {{ props.item.rating.value }}
+        ({{ props.item.rating.count }})
+      </td>
     </template>
   </v-data-table>
 </template>
