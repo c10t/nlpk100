3c06b061c43cbc5081a03501d9b5f4e1149cd2f5
tarrows <c10t@fa.ke>
2020-04-16 23:27:49 +0900

    01 to 03

--- chapter_01/answers.py
+++ chapter_01/answers.py
@@ -1,14 +1,18 @@
+import re
+from typing import List
+
 def q00(s: str) -> str:
     return s[::-1]
 
-def q01():
-    pass
+def q01(s: str) -> str:
+    return "".join(s[::2])
 
-def q02():
-    pass
+def q02(s1: str, s2: str) -> str:
+    return "".join([i + j for i, j in zip(s1, s2)])
 
-def q03():
-    pass
+def q03(s: str) -> List[int]:
+    a_to_z = re.compile(r"[^a-zA-Z]")
+    return [len(a_to_z.sub("", word)) for word in s.split()]
 
 def q04():
     pass
--- chapter_01/test_answers.py
+++ chapter_01/test_answers.py
@@ -7,5 +7,20 @@ class TestChapter01(unittest.TestCase):
         expect = "desserts"
         self.assertEqual(result, expect)
 
+    def test_q01(self):
+        self.assertEqual(q01("パタトクカシーー"), "パトカー")
+
+    def test_q02(self):
+        self.assertEqual(q02("パトカー", "タクシー"), "パタトクカシーー")
+
+    def test_q03(self):
+        result = q03(
+            "Now I need a drink, alcoholic of course, "
+            "after the heavy lectures involving quantum mechanics."
+        )
+        expect = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
+        self.assertEqual(result, expect)
+
+
 if __name__ == '__main__':
     unittest.main()
