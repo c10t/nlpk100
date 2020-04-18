fe18e2fcfb324e49f179c492548709562d0be491
tarrows <c10t@fa.ke>
2020-04-18 23:53:17 +0900

    Q04

--- chapter_01/answers.py
+++ chapter_01/answers.py
@@ -1,5 +1,5 @@
 import re
-from typing import List
+from typing import Dict, List
 
 def q00(s: str) -> str:
     return s[::-1]
@@ -14,8 +14,18 @@ def q03(s: str) -> List[int]:
     a_to_z = re.compile(r"[^a-zA-Z]")
     return [len(a_to_z.sub("", word)) for word in s.split()]
 
-def q04():
-    pass
+def q04(text: str) -> Dict[str, int]:
+    pick_only_first_char = (1, 5, 6, 7, 8, 9, 15, 16, 19)
+    char_to_position = {}
+
+    for i, word in enumerate(text.split()):
+        if i + 1 in pick_only_first_char:
+            char_to_position[word[0:1]] = i + 1
+        else:
+            char_to_position[word[0:2]] = i + 1
+
+    return char_to_position
+
 
 def q05():
     pass
--- chapter_01/test_answers.py
+++ chapter_01/test_answers.py
@@ -21,6 +21,23 @@ class TestChapter01(unittest.TestCase):
         expect = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9]
         self.assertEqual(result, expect)
 
+    def test_q04(self):
+        result = q04(
+            "Hi He Lied Because Boron Could Not Oxidize Fluorine. "
+            "New Nations Might Also Sign Peace Security Clause. "
+            "Arthur King Can."
+        )
+
+        expect = {
+            "H": 1, "He": 2, "Li": 3, "Be": 4, "B": 5, "C": 6,
+            "N": 7, "O": 8, "F": 9, "Ne": 10, "Na": 11, "Mi": 12,
+            "Al": 13, "Si": 14, "P": 15, "S": 16, "Cl": 17,
+            "Ar": 18, "K": 19, "Ca": 20
+        }
+
+        for (k, v) in expect.items():
+            self.assertTrue((k, v) in result.items(), f"{k}:{v} is not found")
+
 
 if __name__ == '__main__':
     unittest.main()
